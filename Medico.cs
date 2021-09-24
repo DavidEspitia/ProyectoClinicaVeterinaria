b7ba72607341d346369b7f36606c4cc8a03f969b
David Espitia <davidespitia@unisinu.edu.co>
2021-09-24 15:09:49 -0500

    Prueba1

---
+++ Medico.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace proyecto1
+
+{
+     class program {
+
+         static void Main (string[] args)
+         {
+             Console.WriteLine("hola mundo");
+             
+         }
+     }
+}
